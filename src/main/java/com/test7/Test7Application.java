3cd4df5d60047a4fcdfda58515ab00ad78bf9d10
prema <premausn4ub20ee036@gmail.com>
2025-04-25 15:27:27 +0530

    Second Commit

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test7Application {
 
 	public static void main(String[] args) {
+              int x=100;
 		SpringApplication.run(Test7Application.class, args);
 	}
 
