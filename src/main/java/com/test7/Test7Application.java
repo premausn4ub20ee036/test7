a85561692805c6d70d3107ad63b8f9cca06955ae
premausn4ub20ee036 <premausn4ub20ee036@gmail.com>
2025-04-25 15:55:33 +0530

    Update Test7Application.java

--- src/main/java/com/test7/Test7Application.java
+++ src/main/java/com/test7/Test7Application.java
@@ -9,7 +9,7 @@ public class Test7Application {
 	public static void main(String[] args) {
               String name="mike";
 	      int x=100;
-	       int y=900;
+	       int y=300;
 		SpringApplication.run(Test7Application.class, args);
 	}
 
